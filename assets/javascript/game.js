6b6df29d81d78566283eb9a4d217648f34977b9e
julioSanchez <juliosanchez@Julios-MBP.local>
2018-08-26 09:55:31 -0500

    trying to add a reset button that hides onclick

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 //create my variables 
 var win = 0;
 var lose = 0;
-var wordsLeft = 9;
+var lettersLeft = 9;
 var usedLetters = [];
 
 //create random inex and apply it to the letters array to select a random letter
@@ -19,15 +19,20 @@ function newLetter(){
 
 //Here is where I will track my guesses left
 function reduceWordsLeft(){
-    document.getElementById('guessesLeft').innerHTML = wordsLeft;
+    document.getElementById('guessesLeft').innerHTML = lettersLeft;
 }
 //This function will be called to get things back to a new game
 function reset(){
-    wordsLeft = 9;
+    lettersLeft = 9;
     usedLetters = [];
     newLetter();
     reduceWordsLeft();
     document.getElementById('currentGuesses').innerHTML = ' ';
+    
+}
+
+function playAgain(){
+    document.getElementById('playAgainButton').style.display = 'block'; 
 }
 
 
@@ -43,20 +48,19 @@ document.onkeyup = (event) =>{
     if( pressedKey === randomLetter ){
         win++;
         document.getElementById('wins').innerHTML = win;
-
+        playAgain();
         reset();
-        console.log('match');
     }   
     //Do this if player chooses the wrong letter
     if( pressedKey !== randomLetter ){
         //Now I have to .push the selected letters to the usedLetters array
         usedLetters.push(pressedKey);
-        wordsLeft--;
+        lettersLeft--;
         document.getElementById('currentGuesses').innerHTML = usedLetters;
         reduceWordsLeft();   
     }
 
-    if ( wordsLeft === 0 ){
+    if ( lettersLeft === 0 ){
         lose++;
         document.getElementById('loses').innerHTML = lose;
         alert('Sorry the correct letter was ' + randomLetter);
@@ -67,6 +71,6 @@ document.onkeyup = (event) =>{
 } /*closing bracket for document.onkeyup*/
 
 //these allow the numbers to be seen befroe any functions are run
-document.getElementById('guessesLeft').innerHTML = wordsLeft;
+document.getElementById('guessesLeft').innerHTML = lettersLeft;
 document.getElementById('wins').innerHTML = win;
 document.getElementById('loses').innerHTML = lose;
--- assets/javascript/games2.js
+++
@@ -1,4 +0,0 @@
-var wins = 0;
-var loses = 0;
-var choicesLeft = 9;
-var usedWords = [];
