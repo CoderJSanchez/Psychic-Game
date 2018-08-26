0f1a750f0d6de2b2223566968bf9e2ac5176ded5
julioSanchez <juliosanchez@Julios-MBP.local>
2018-08-26 07:53:36 -0500

    refactored all my code and added an alert with the correct answer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,66 @@
 //array of computer choices
-let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
+//create my variables 
+var win = 0;
+var lose = 0;
+var wordsLeft = 9;
+var usedLetters = [];
 
-let wins = 0;
-let loses = 0;
-let guessesLeft = 9;
-const randomNumber = Math.floor(Math.random() * letters.length);
-const randomLetter =letters[randomNumber];
+//create random inex and apply it to the letters array to select a random letter
+var randomLetter = letters[Math.floor(Math.random() * letters.length)];
     console.log(randomLetter);
-let usedLetters = [];
 
-let playerGuess = document.onkeyup = () =>{
-    const pickedLetter = event.key;
-    
-    usedLetters.push(pickedLetter);
-    console.log(pickedLetter);
-    console.log(usedLetters);   
-
-    if(pickedLetter === randomLetter){
-        wins++;
-        document.getElementById('wins').innerHTML = wins;
-        reset();
-        
-    }
-
-    if(pickedLetter !== randomLetter){
-        guessesLeft--;
-        document.getElementById('guessesLeft').innerHTML = guessesLeft;
-    }
-
-    if(guessesLeft < 1){
-        loses++;
-        reset();
-    }
-    document.getElementById('currentGuesses').innerHTML = usedLetters;
-    document.getElementById('loses').innerHTML = loses;
-    
-    
+//This function is called when a new letter is needed after a win or loss
+function newLetter(){
+    randomLetter = letters[Math.floor(Math.random() * letters.length)];
 }
-document.getElementById('guessesLeft').innerHTML = guessesLeft;
 
+//Here is where I will track my guesses left
+function reduceWordsLeft(){
+    document.getElementById('guessesLeft').innerHTML = wordsLeft;
+}
+//This function will be called to get things back to a new game
 function reset(){
-    guessesLeft = 9;
+    wordsLeft = 9;
     usedLetters = [];
+    newLetter();
+    reduceWordsLeft();
 }
 
 
+//Now I need to keep track the letters chosen by the player by 'reading' their keypresses
+document.onkeyup = (event) =>{
+    var pressedKey = event.key;
+    console.log(usedLetters);
+    console.log(pressedKey);
+
+    //Begin logic for letters chosen. FYI, notice this is still inside of the document.onkeyup
+
+    //Do this if player guesses the letter
+    if( pressedKey === randomLetter ){
+        win++;
+        document.getElementById('wins').innerHTML = win;
+
+        reset();
+        console.log('match');
+    }   
+    //Do this if player chooses the wrong letter
+    if( pressedKey !== randomLetter ){
+        //Now I have to .push the selected letters to the usedLetters array
+        usedLetters.push(pressedKey);
+        wordsLeft--;
+        document.getElementById('currentGuesses').innerHTML = usedLetters;
+        reduceWordsLeft();   
+    }
+
+    if ( wordsLeft < 1 ){
+        reset();
+        lose++;
+        document.getElementById('loses').innerHTML = lose;
+        alert('Sorry the correct letter was ' + randomLetter);
+
+    }
+
+} /*closing bracket for document.onkeyup*/
---
+++ assets/javascript/games2.js
@@ -0,0 +1,4 @@
+var wins = 0;
+var loses = 0;
+var choicesLeft = 9;
+var usedWords = [];
